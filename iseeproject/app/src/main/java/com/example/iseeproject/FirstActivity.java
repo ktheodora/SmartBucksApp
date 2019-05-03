adc33d7b7d56a3cf5dfed9c5784c7b0c8a8ac50b
it-tct <it@thecretetrip.org>
2019-05-03 08:32:04 +0200

    added java for first activity

---
+++ iseeproject/app/src/main/java/com/example/iseeproject/FirstActivity.java
@@ -0,0 +1,12 @@
+package com.example.iseeproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+
+    }
+}
