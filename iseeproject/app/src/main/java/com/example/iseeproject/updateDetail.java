2bc217aa73794f21944ae7c1a0862fec271abd40
Pawan Kumar <pawan.kumar@st.ovgu.de>
2019-05-13 13:29:10 +0200

    Updated Prototype

--- iseeproject/app/src/main/java/com/example/iseeproject/updateDetail.java
+++ iseeproject/app/src/main/java/com/example/iseeproject/updateDetail.java
@@ -1,13 +1,32 @@
 package com.example.iseeproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class updateDetail extends AppCompatActivity {
-
+        private Button update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_detail);
+
+        update=(Button)findViewById(R.id.updtbtn);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update();
+            }
+        });
     }
+
+        private void update()
+        {
+            Intent intent2 = new Intent(updateDetail.this , HomePage.class);
+            startActivity(intent2);
+
+        }
 }
+
