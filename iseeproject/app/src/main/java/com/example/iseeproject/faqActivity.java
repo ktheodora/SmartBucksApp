184631a26cc21a0ce24425ea7ac929e4e3b30b1a
Venkatesh Murugadas <venkateshdas05@gmail.com>
2019-06-28 22:27:38 +0200

    Updated faq

--- iseeproject/app/src/main/java/com/example/iseeproject/faqActivity.java
+++ iseeproject/app/src/main/java/com/example/iseeproject/faqActivity.java
@@ -6,12 +6,16 @@ import android.webkit.WebView;
 
 public class faqActivity extends AppCompatActivity {
     WebView webView;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        //Get a reference to your WebView//
+        webView = (WebView) findViewById(R.id.webView1);
+
+//Specify the URL you want to display//
+        webView.loadUrl("file:///android_asset/faq1.html");
+
     }
+
 }
